95eeceb8ec27b46fb2fe36200ee580e15f36b817
Bejal Lewis <bejal.lewis@grafana.com>
2025-11-04 18:05:08 +0100

    Add basic alloy engine

--- collector/generate.go
+++ collector/generate.go
@@ -1,6 +1,6 @@
 package main
 
-//go:generate go run ./generator/generate_replaces.go .. ./builder-config.yaml
+//go:generate go run ./generator/generate_replaces.go .. ./builder-config.yaml ../extension/alloyengine/go.mod
 //go:generate builder --config ./builder-config.yaml --skip-compilation
-//go:generate go mod tidy
+//go:generate sh -c "go mod tidy && cd ../extension/alloyengine && go mod tidy"
 //go:generate go run ./generator/generator.go -- ./main.go ./main_alloy.go
