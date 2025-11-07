ec004f0a34733ade954a3694c1fdf317c9c6494b
Shirley Leu <4163034+fridgepoet@users.noreply.github.com>
2025-11-07 10:03:15 +0100

    put back import

--- internal/component/database_observability/postgres/component_test.go
+++ internal/component/database_observability/postgres/component_test.go
@@ -9,7 +9,6 @@ import (
 	"github.com/stretchr/testify/require"
 
 	kitlog "github.com/go-kit/log"
-
 	cmp "github.com/grafana/alloy/internal/component"
 	"github.com/grafana/alloy/internal/component/common/loki"
 	loki_fake "github.com/grafana/alloy/internal/component/common/loki/client/fake"
